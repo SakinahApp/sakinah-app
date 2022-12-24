f9846807fb0d5cc519ac974002356d0b2ede15ad
nekruza <nekruz.avgani@gmail.com>
2022-12-24 19:24:14 +0000

    user preference files

---
+++ src/views/sakinah/User/Therapists/ChosenPrefrences.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function ChosenPrefrences({ item }) {
+  return (
+    <div
+      style={{
+        padding: "10px",
+        fontWeight: 600,
+        width: "100%",
+        maxWidth: 150,
+        margin: 10,
+        borderRadius: 10,
+        height: 50,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "column",
+        background: "#f5f5f5",
+        color: "rgb(50, 51, 49)",
+        border: "1px solid rgb(95, 106, 196, 1)",
+        borderColor: "white",
+        fontSize: 16,
+      }}
+    >
+      {item} 😔
+    </div>
+  );
+}
+
+export default ChosenPrefrences;
