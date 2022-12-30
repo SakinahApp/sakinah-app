5ad0ea187e2425d921265bebe7fffed8c8e9d218
nekruza <nekruz.avgani@gmail.com>
2022-12-30 02:13:54 +0000

    Cancel button +  prev and upcoming session filter

--- src/views/sakinah/User/MySessions/PrevSessions.tsx
+++ src/views/sakinah/User/MySessions/PrevSessions.tsx
@@ -9,28 +9,22 @@ import Paper from "@mui/material/Paper";
 import { Avatar } from "@mui/material";
 import therapistImage from "../images/therapist1.png";
 import CssBaseline from "@mui/material/CssBaseline";
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Session #1", 159, 6.0, 24, 4.0),
-  createData("Session #2", 237, 9.0, 37, 4.3),
-  createData("Session #3", 262, 16.0, 24, 6.0),
-  createData("Session #4", 305, 3.7, 67, 4.3),
-  createData("Session #5", 305, 3.7, 67, 4.3),
-  createData("Session #6", 305, 3.7, 67, 4.3),
-  createData("Session #7", 356, 16.0, 49, 3.9),
-];
+import { useStore } from "../../../../Zustand";
+import dayjs from "dayjs";
 
 export default function PrevSessions() {
+  const { upcomingSessionState } = useStore((state) => state);
+
+  // Filtering previous sessions
+  const now = dayjs().unix();
+
+  const prevSessions =
+    upcomingSessionState?.length > 0 &&
+    upcomingSessionState?.filter(
+      (session) =>
+        dayjs(session.date + " " + session.time?.slice(0, 5)).unix() < now
+    );
+
   return (
     <TableContainer
       component={Paper}
@@ -71,43 +65,57 @@ export default function PrevSessions() {
               style={{ color: "white", fontWeight: 600, fontSize: 15 }}
               align="center"
             >
+              Time
+            </TableCell>
+            <TableCell
+              style={{ color: "white", fontWeight: 600, fontSize: 15 }}
+              align="center"
+            >
               Duration
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
+          {prevSessions?.length > 0 &&
+            prevSessions
+              ?.sort(
+                (a, b) =>
+                  dayjs(b.date + " " + b.time?.slice(0, 5)).unix() -
+                  dayjs(a.date + " " + a.time?.slice(0, 5)).unix()
+              )
+              ?.map((row, index) => (
+                <TableRow
+                  key={row.docId}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    Session {index + 1}
+                  </TableCell>
 
-              <TableCell
-                align="center"
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Avatar
-                  src={therapistImage}
-                  sx={{
-                    width: 35,
-                    height: 35,
-                    border: "2px solid rgb(95, 106, 196)",
-                    marginRight: 1.5,
-                  }}
-                />
-                <p>Abdullah Gul</p>
-              </TableCell>
-              <TableCell align="center">December 10, 2022</TableCell>
-              <TableCell align="center">50 min</TableCell>
-            </TableRow>
-          ))}
+                  <TableCell
+                    align="center"
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Avatar
+                      src={therapistImage}
+                      sx={{
+                        width: 35,
+                        height: 35,
+                        border: "2px solid rgb(95, 106, 196)",
+                        marginRight: 1.5,
+                      }}
+                    />
+                    <p>{row.therapist_name}</p>
+                  </TableCell>
+                  <TableCell align="center">{row?.date}</TableCell>
+                  <TableCell align="center">{row?.time}</TableCell>
+                  <TableCell align="center">50 min</TableCell>
+                </TableRow>
+              ))}
         </TableBody>
       </Table>
     </TableContainer>
