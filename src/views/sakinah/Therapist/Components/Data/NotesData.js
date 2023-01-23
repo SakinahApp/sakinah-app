2a427b9ecf93739ed4a7f02e5bef89c736cb7867
Akmal Azami <akmal@azami.io>
2023-01-23 21:07:47 +0000

    Changed data structure by DummyData.js into separate categories

---
+++ src/views/sakinah/Therapist/Components/Data/NotesData.js
@@ -0,0 +1,168 @@
+const notesData = [
+	{
+		therapist_id: 11,
+		user_id: 1,
+		nameUser: "Salih Uddin",
+		nameTherapist: "Abdullah Arabi",
+		date: "12 June 2022",
+		note:
+			"Hello, this is my first session and don't know where to start for this bla bla",
+		writtenBy: "user",
+	},
+	{
+		therapist_id: 11,
+		user_id: 1,
+		nameUser: "Salih Uddin",
+		nameTherapist: "Abdullah Arabi",
+		date: "12 June 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+	{
+		therapist_id: 12,
+		user_id: 1,
+		nameUser: "Salih Uddin",
+		nameTherapist: "Farrukh Negma",
+		date: "12 June 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+	{
+		therapist_id: 11,
+		user_id: 1,
+		nameUser: "Sabrina Salaha",
+		nameTherapist: "Abdullah Arabi",
+		date: "12 June 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+	{
+		therapist_id: 11,
+		user_id: 2,
+		nameUser: "Sabrina Salaha",
+		nameTherapist: "Abdullah Arabi",
+		date: "23 June 2023",
+		note:
+			"Hello, this is my first session and don't know where to start for this bla bla",
+		writtenBy: "user",
+	},
+
+	{
+		therapist_id: 13,
+		user_id: 2,
+		nameUser: "Sabrina Salaha",
+		nameTherapist: "Amina Khan",
+		date: "12 June 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+	{
+		therapist_id: 12,
+		user_id: 2,
+		nameUser: "Sabrina Salaha",
+		nameTherapist: "Farrukh Negma",
+		date: "12 June 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+	{
+		therapist_id: 11,
+		user_id: 2,
+		nameUser: "Sabrina Salaha",
+		nameTherapist: "Abdullah Arabi",
+		date: "12 June 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+
+	{
+		therapist_id: 14,
+		user_id: 3,
+		nameUser: "Andrew Tatov",
+		nameTherapist: "Samira Hassani",
+		date: "14 June 2022",
+		note:
+			"Hello, this is my first session and don't know where to start for this bla bla",
+		writtenBy: "user",
+	},
+	{
+		therapist_id: 14,
+		user_id: 3,
+		nameUser: "Andrew Tatov",
+		nameTherapist: "Samira Hassani",
+		date: "14 June 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+	{
+		therapist_id: 12,
+		user_id: 3,
+		nameUser: "Andrew Tatov",
+		nameTherapist: "Farrukh Negma",
+		date: "14 June 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+	{
+		therapist_id: 11,
+		user_id: 3,
+		nameUser: "Sabrina Salaha",
+		nameTherapist: "Abdullah Arabi",
+		date: "12 June 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+
+	{
+		therapist_id: 15,
+		user_id: 4,
+		nameUser: "Andrew Tatov",
+		nameTherapist: "Hussein Salim",
+		date: "30 June 2022",
+		note:
+			"Hello, this is my first session and don't know where to start for this bla bla",
+		writtenBy: "user",
+	},
+
+	{
+		therapist_id: 15,
+		user_id: 4,
+		nameUser: "Andrew Tatov",
+		nameTherapist: "Hussein Salim",
+		date: "30 July 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+	{
+		therapist_id: 15,
+		user_id: 4,
+		nameUser: "Andrew Tatov",
+		nameTherapist: "Hussein Salim",
+		date: "30 August 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+	{
+		therapist_id: 11,
+		user_id: 4,
+		nameUser: "Andrew Tatov",
+		nameTherapist: "Abdullah Arabi",
+		date: "30 October 2022",
+		note:
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ea consectetur obcaecati nihil aperiam.",
+		writtenBy: "therapist",
+	},
+];
+
+export default notesData;
