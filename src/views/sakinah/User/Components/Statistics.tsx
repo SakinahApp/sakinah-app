0d0db651ba2ceb7f79342cc59f25a14451cfc8cb
nekruza <nekruz.avgani@gmail.com>
2023-01-25 23:50:10 +0000

    Updated stats component

--- src/views/sakinah/User/Components/Statistics.tsx
+++ src/views/sakinah/User/Components/Statistics.tsx
@@ -2,6 +2,10 @@ import { Box } from "@mui/material";
 import React from "react";
 import { useStore } from "../../../../Zustand";
 import dayjs from "dayjs";
+import AddModeratorIcon from "@mui/icons-material/AddModerator";
+import GppBadIcon from "@mui/icons-material/GppBad";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PaidIcon from "@mui/icons-material/Paid";
 
 function Statistics(props) {
   const { upcomingSessionState } = useStore((state) => state);
@@ -23,21 +27,35 @@ function Statistics(props) {
   const data = [
     {
       name: "Total # of Sessions",
+      color: "#e2e6fb4d",
       number: upcomingSessionState?.length,
-    },
-    {
-      name: "Cancelled Sessions",
-      number: cancelledSessions?.length,
+      icon: (
+        <AddModeratorIcon
+          style={{ width: 50, height: 50, color: "rgb(95, 97, 196)" }}
+        />
+      ),
     },
     {
       name: "Previous Sessions",
+      color: "rgb(65 182 255 / 8%)",
       number: prevSessions?.length,
+      icon: (
+        <SkipPreviousIcon style={{ width: 50, height: 50, color: "#41b6ff" }} />
+      ),
+    },
+    {
+      name: "Cancelled Sessions",
+      color: "rgb(255 139 79 / 7%)",
+      number: cancelledSessions?.length,
+      icon: <GppBadIcon style={{ width: 50, height: 50, color: "#ff8b4f" }} />,
     },
     {
       name: "Amount Paid",
+      color: "rgb(94 196 151 / 8%)",
       number: `£${
         (upcomingSessionState?.length - cancelledSessions?.length) * 50
       }`,
+      icon: <PaidIcon style={{ width: 50, height: 50, color: "#5fc497" }} />,
     },
   ];
 
@@ -72,24 +90,29 @@ function Statistics(props) {
               color: "white",
               height: 100,
               display: "flex",
-              justifyContent: "space-between",
+              justifyContent: "space-evenly",
               alignItems: "center",
-              flexDirection: "column",
-              background: "#E8E2FB",
+              flexDirection: "row",
+              boxShadow:
+                "rgb(50 50 93 / 5%) 0px 2px 5px -1px, rgb(0 0 0 / 20%) 0px 1px 3px -1px",
+              background: item.color,
             }}
           >
-            <p style={{ fontSize: 15, color: "rgba(50, 51, 49, 0.8)" }}>
-              {item.name}
-            </p>
-            <h2
-              style={{
-                fontWeight: 600,
-                fontSize: 22,
-                color: "rgb(50, 51, 49)",
-              }}
-            >
-              {item.number}
-            </h2>
+            {item.icon}
+            <div>
+              <h2
+                style={{
+                  fontWeight: 600,
+                  fontSize: 22,
+                  color: "rgb(50, 51, 49)",
+                }}
+              >
+                {item.number}
+              </h2>
+              <p style={{ fontSize: 15, color: "rgba(50, 51, 49, 0.8)" }}>
+                {item.name}
+              </p>
+            </div>
           </div>
         ))}
       </div>
