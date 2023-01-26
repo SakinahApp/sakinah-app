84f3e86a0e1f52dc99667b532ff64078d1df205c
nekruza <nekruz.avgani@gmail.com>
2023-01-26 18:17:35 +0000

    Updated avatar

--- src/views/auth/User/SignOut.tsx
+++ src/views/auth/User/SignOut.tsx
@@ -15,6 +15,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { useStoreUser } from "../../../Zustand";
 import { Button } from "@mui/material";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+import PersonIcon from "@mui/icons-material/Person";
 
 export default function AccountMenu() {
   const navigate = useNavigate();
@@ -65,16 +68,23 @@ export default function AccountMenu() {
             aria-haspopup="true"
             aria-expanded={open ? "true" : undefined}
           >
-            <Avatar
-              sx={{
-                width: 34,
-                height: 34,
-                border: "2px solid rgb(95, 106, 196)",
-                background: "rgba(95, 106, 196, 0.05)",
-              }}
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
             >
-              {userInfo?.name?.slice(0, 1)}
-            </Avatar>
+              <Avatar
+                sx={{
+                  width: 34,
+                  height: 34,
+                  border: "2px solid rgb(95, 106, 196)",
+                  background: "rgba(95, 106, 196, 0.05)",
+                  color: "rgb(95, 106, 196)",
+                }}
+              >
+                <PersonIcon />
+              </Avatar>
+            </StyledBadge>
           </IconButton>
         </Tooltip>
       </Box>
@@ -136,3 +146,32 @@ export default function AccountMenu() {
     </React.Fragment>
   );
 }
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
