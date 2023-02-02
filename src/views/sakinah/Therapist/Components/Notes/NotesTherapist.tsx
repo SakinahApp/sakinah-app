4219b63a186de397703ed93a5dacd31f8c4046db
Akmal Azami <akmal@azami.io>
2023-02-02 18:58:48 +0000

    Add, edit, delete notes | dayjs used

--- src/views/sakinah/Therapist/Components/Notes/NotesTherapist.tsx
+++ src/views/sakinah/Therapist/Components/Notes/NotesTherapist.tsx
@@ -1,35 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../Data/DummyData";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import AddNotes from "./AddNotes";
-const NotesTherapist = () => {
+import notesData from "../Data/NotesData";
+import SingleNoteTherapist from "./SingleNoteTherapist";
+import dayjs from "dayjs";
+
+const NotesTherapist = ({ client }) => {
+	const [notes, setNotes] = useState(notesData);
+	const [inputNote, setInputNote] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setNotes([
+			...notes,
+			{
+				therapist_id: "11",
+				user_id: client.user_id,
+				nameUser: client.user_name,
+				note_id: client.note_id,
+				nameTherapist: "Abdullah Arabi",
+				date: dayjs().format("DD MMMM, YYYY, HH:mm"),
+				note: inputNote,
+				writtenBy: "therapist",
+			},
+		]);
+		setInputNote("");
+	};
+
 	return (
-		<div>
+		<>
 			<div className="bg-pink-50 p-5 mb-5">Notes from the therapists</div>
 			<div className="mb-5">
-				<AddNotes placeholder="Please write the note here, notes are not visible for the client " />
+				{/* <AddNotes placeholder="Please write the note here, notes are not visible for the client " /> */}
+
+				<AddNotes
+					handleSubmit={handleSubmit}
+					inputNote={inputNote}
+					setInputNote={setInputNote}
+					btn="Save Note"
+				/>
+				{notes
+					.filter(
+						(note) =>
+							note.user_id === client.user_id && note.writtenBy === "therapist"
+					)
+					.map((note) => (
+						<SingleNoteTherapist note={note} notes={notes} setNotes={setNotes} />
+					))}
 			</div>
-
-			<button className="btn-primary mb-5">Save Note</button>
-
-			{data[0].notes.map((tag) => (
-				<div className="text-sm min-w-full  inline-block  p-5 mb-5  text-stone-800 bg-yellow-50 border-l-black  border-l-2 last:mr-0 mr-1">
-					<div className="flex ">
-						<div className="text-xs text-gray-700 mb-2 cursor-pointer">
-							<p> {tag.nameTherapist}</p>
-							<p> {tag.date}</p>
-						</div>
-						<div className="flex right-0 ml-5 gap-2 cursor-pointer">
-							<EditIcon style={{}} />
-							<DeleteOutlineIcon />
-						</div>
-					</div>
-
-					<p> {tag.note}</p>
-				</div>
-			))}
-		</div>
+		</>
 	);
 };
 
--- src/views/sakinah/Therapist/Components/TabCustomer.tsx
+++ src/views/sakinah/Therapist/Components/TabCustomer.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import data from "./Data/DummyData";
 import NotesTherapist from "./Notes/NotesTherapist";
 import NotesClient from "./Notes/NotesClient";
 import TabComponent from "./TabComponent";
 
-const TabsCustomer = ({ color, tab1, tab2 }) => {
+const TabsCustomer = ({ color, tab1, tab2, client }) => {
 	const [openTab, setOpenTab] = React.useState(1);
 	return (
 		<>
@@ -17,21 +16,21 @@ const TabsCustomer = ({ color, tab1, tab2 }) => {
 						openTab={openTab}
 						setOpenTab={setOpenTab}
 					/>
-					<div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+					<div className="relative flex flex-col text-sm min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
 						<div className="px-4 py-5 flex-auto">
 							<div className="tab-content tab-space">
 								<div className={openTab === 1 ? "block" : "hidden"} id="link1">
 									{/* Grid of profile information of the user : Phone, email, location */}
 									<div className="grid grid-cols-2  gap-3 ">
 										<div className="text-gray-500">Phone:</div>
-										<div>{data[0].phone}</div>
+										<div>{client.phone}</div>
 										<div className="text-gray-500">Email:</div>
-										<div>{data[0].email}</div>
+										<div>{client.email}</div>
 										<div className="text-gray-500">Location:</div>
-										<div>{data[0].location}</div>
+										<div>{client.location}</div>
 										<div className="text-gray-500">Tags</div>
 										<div>
-											{data[0].tags.map((tag) => (
+											{client.tags.map((tag) => (
 												<span className="text-sm font-semibold inline-block m-1 py-1 px-2 uppercase rounded text-sky-700 bg-sky-200 last:mr-0 mr-1">
 													{" "}
 													{tag}
@@ -41,8 +40,8 @@ const TabsCustomer = ({ color, tab1, tab2 }) => {
 									</div>
 								</div>
 								<div className={openTab === 2 ? "block" : "hidden"} id="link2">
-									<NotesClient />
-									<NotesTherapist />
+									<NotesClient client={client} />
+									<NotesTherapist client={client}/>
 								</div>
 							</div>
 						</div>
