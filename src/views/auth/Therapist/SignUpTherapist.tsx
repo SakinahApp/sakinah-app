ef4972804edbc5067790bc21705d66fdf4b337b3
Akmal Azami <akmal@azami.io>
2023-03-18 12:53:28 +0400

    Removing separate firbase for therapist and having only one firebase for therapist and patient

--- src/views/auth/Therapist/SignUpTherapist.tsx
+++ src/views/auth/Therapist/SignUpTherapist.tsx
@@ -1,146 +1,140 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { NavLink } from "react-router-dom";
-import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
-import { useState } from 'react';
-import { authTherapist } from "../../../FirebaseTherapist";
-import { Alert } from '@mui/material';
-
+import {
+	createUserWithEmailAndPassword,
+	sendEmailVerification,
+} from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../../../Firebase";
+import { Alert } from "@mui/material";
 
 function Copyright(props: any) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://sakinah.app/">
-Sakinah      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+	return (
+		<Typography variant="body2" color="text.secondary" align="center" {...props}>
+			{"Copyright© "}
+			<Link color="inherit" href="https://sakinah.app/">
+				Sakinah{" "}
+			</Link>{" "}
+			{new Date().getFullYear()}
+			{"."}
+		</Typography>
+	);
 }
 
 const theme = createTheme();
 
-
 export default function SignUpTherapist() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [message, setMessage] = useState("")
-  
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [message, setMessage] = useState("");
 
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    
-    createUserWithEmailAndPassword(authTherapist, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        
-        sendEmailVerification(user)
-          
-      .then(() => {
-        // Email verification sent!
-        setMessage("Email verification sent! Please check your email including SPAM folder. After verified your account, you can log in.");
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				const user = userCredential.user;
 
-      });
+				sendEmailVerification(user).then(() => {
+					// Email verification sent!
+					setMessage(
+						"Email verification sent! Please check your email including SPAM folder. After verified your account, you can log in."
+					);
+				});
 
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        
-        if (errorCode === "auth/email-already-in-use")
-        setMessage("Email address already exists")
-        // ..
-      });
-  };
+				// ...
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
 
-  return (
-    <>
-    <CssBaseline />
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  value={email}
-                  onChange={(e)=>setEmail(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  value={password}
-                  onChange={(e)=>setPassword(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-              {message && <Alert severity="success">{message}</Alert>}
-            </Grid>
-             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="center"  >
-              <Grid item >
-                <NavLink to="/auth/therapists/signin" >
-                Already have an account?
-                <span style={{ textDecoration: "underline", color: "#1976d2" }}>
-                  Sign in
-                  </span>
-                </NavLink>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </>
-  );
+				if (errorCode === "auth/email-already-in-use")
+					setMessage("Email address already exists");
+				// ..
+			});
+	};
+
+	return (
+		<>
+			<CssBaseline />
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign up
+					</Typography>
+					<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									id="email"
+									label="Email Address"
+									name="email"
+									autoComplete="email"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									name="password"
+									label="Password"
+									type="password"
+									id="password"
+									autoComplete="new-password"
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								{message && <Alert severity="success">{message}</Alert>}
+							</Grid>
+						</Grid>
+						<Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+							Sign Up
+						</Button>
+						<Grid container justifyContent="center">
+							<Grid item>
+								<NavLink to="/auth/therapists/signin">
+									Already have an account?
+									<span style={{ textDecoration: "underline", color: "#1976d2" }}>
+										Sign in
+									</span>
+								</NavLink>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+				<Copyright sx={{ mt: 5 }} />
+			</Container>
+		</>
+	);
 }
